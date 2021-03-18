95625f6d7bce66a801e660e1c0b29b851ca2d48a
Ahnaf Ahmed <43047641+ahnafone@users.noreply.github.com>
2021-03-18 15:39:51 +0600

    Updates

---
+++ precache-manifest.e1912b66b3578315e646749dceadb3d8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f0dc1c23154605d0cc0ac2dea3c2d974",
+    "url": "/React-TodoList-App/index.html"
+  },
+  {
+    "revision": "a73dd1222a476b2d453e",
+    "url": "/React-TodoList-App/static/js/2.e02bef4b.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/React-TodoList-App/static/js/2.e02bef4b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "af7013e57c0579b0e661",
+    "url": "/React-TodoList-App/static/js/main.314956ff.chunk.js"
+  },
+  {
+    "revision": "c159621884add44cf1b0",
+    "url": "/React-TodoList-App/static/js/runtime-main.d9031019.js"
+  }
+]);
